4b1ec0b0f16e62a1d22d22d8c1810a2afffbc788
tommyxlanes <tommyxlanes@gmail.com>
2024-08-05 21:06:30 -0700

    first commit

--- app/components/SwiperCarousel.tsx
+++ app/components/SwiperCarousel.tsx
@@ -1,39 +1,48 @@
-'use client'
+'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-import {
-    FaTruck,
-    FaFire,
-    FaTag
-} from 'react-icons/fa'
+import { FaTruck, FaFire, FaTag } from 'react-icons/fa';
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 import Link from 'next/link';
 // Import Swiper styles
 
 export default () => {
   return (
     <Swiper
-    modules={[Autoplay, Pagination]}
-    slidesPerView={1}
-    // autoplay={{
-    //       delay: 2500,
-    //       pauseOnMouseEnter: true,
-    //       disableOnInteraction: false
-    //      }}
-    loop
-    className='swiper-container'
-    >
-      <SwiperSlide className='text-center flex justify-center items-center gap-2 text-[#666]'>
-        <FaTruck className='text-xl text-[orangered]'/> Free Shipping
-        <Link href="/" className='px-4 py-[4px] rounded-full text-xs bg-[#327739] text-white'>Shop Now</Link>
+      modules={[Autoplay, Pagination]}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: false,
+      }}
+      loop
+      className="swiper-container">
+      <SwiperSlide className="mt-4">
+        <div className="text-center flex justify-center items-center gap-2 text-[#666]">
+          <FaTruck className="text-xl text-[orangered]" />
+          <p>Free Shipping $50+</p>
+          <Link
+            href="/"
+            className="px-4 py-[4px] rounded-full text-xs bg-[#327739] text-white">
+            Shop Now
+          </Link>
+        </div>
       </SwiperSlide>
-      <SwiperSlide className='text-center flex justify-center items-center gap-2 text-[#666]'>
-       <FaTag className='text-xl text-[orangered]'/> Get 20% off your first purchase
-      <Link href="/" className='px-4 py-[4px] rounded-full text-xs bg-[#327739] text-white'>Claim Now</Link>
+      <SwiperSlide className="mt-4">
+        <div className="text-center flex justify-center items-center gap-2 text-[#666]">
+          <FaTruck className="text-xl text-[orangered]" />
+          <p>Get 20% off your first purchase</p>
+          <Link
+            href="/"
+            className="px-4 py-[4px] rounded-full text-xs bg-[#327739] text-white">
+            Claim Now
+          </Link>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,117 @@
-'use client'
+'use client';
 
-import Image from "next/image";
-import { Carousel } from "./components/Carousel";
-import InfoCard from "./components/InfoCard";
-import NewsLetter from "./components/NewsLetter";
-import Subscribe from "./components/Subscribe";
+import Image from 'next/image';
+import { Carousel } from './components/Carousel';
+import InfoCard from './components/InfoCard';
+import NewsLetter from './components/NewsLetter';
+import Subscribe from './components/Subscribe';
 
-import { motion } from 'framer-motion'
-import SwiperCarousel from "./components/SwiperCarousel";
+import { motion } from 'framer-motion';
+import SwiperCarousel from './components/SwiperCarousel';
 
-import styles from './page.module.css'
-import Icons from "./components/Icons";
-import NewArrival from "./components/NewArrival";
+import styles from './page.module.css';
+import Icons from './components/Icons';
+import NewArrival from './components/NewArrival';
 
 const pens = [
   {
     title: 'Pen',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/pen-info.png',
-      '/images/pen-info-left.png'
-    ]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/pen-info.png', '/images/pen-info-left.png'],
   },
   {
     title: 'Pen',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/pen-info.png',
-      '/images/pen-info-left.png'
-    ]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/pen-info.png', '/images/pen-info-left.png'],
   },
   {
     title: 'Pen',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/pen-info.png',
-      '/images/pen-info-left.png'
-    ]
-  }
-]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/pen-info.png', '/images/pen-info-left.png'],
+  },
+];
 
 const edibles = [
   {
     title: 'Edibles',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/edibles.png',
-      '/images/edibles-left.png'
-    ]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/edibles.png', '/images/edibles-left.png'],
   },
   {
     title: 'Edibles',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/edibles.png',
-      '/images/edibles-left.png'
-    ]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/edibles.png', '/images/edibles-left.png'],
   },
   {
     title: 'Edibles',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
-    imageUrl: [
-      '/images/edibles.png',
-      '/images/edibles-left.png'
-    ]
-  }
-]
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ipsam modi dolorum ipsa similique vero numquam fuga perspiciatis eius officiis ratione blanditiis autem vel.',
+    imageUrl: ['/images/edibles.png', '/images/edibles-left.png'],
+  },
+];
 
 export default function Home() {
   return (
     <main>
-        <div className="hidden 2xl:flex gap-4 p-4 bg-[#f5f5f5] w-[100%]">
-         <div className="h-96 w-1/4">
-          <img className="h-full w-full object-cover object-center" src="images/BD-FireDeals_Mobile.png" alt="" />
-         </div>
-         <div className="h-96 flex-2">
-          <img className="h-full w-full object-cover" src="images/BD-E-Torch-x-Dozo-B2SH_Desktop-Banner.png" alt="" />
-         </div>
-         <div className="h-96 w-1/4">
-          <img className="h-full w-full object-cover object-center" src="images/BD-FireDeals_Mobile.png" alt="" />
-         </div>
+      <div className="hidden xl:flex gap-4 p-4 bg-[#f5f5f5] w-[100%]">
+        <div className="h-96 w-1/4">
+          <img
+            className=" w-full h-full object-cover object-left-bottom"
+            src="images/BD-FireDeals_Mobile.png"
+            alt=""
+          />
         </div>
-        {
-          <div className="flex gap-4 p-4 2xl:hidden bg-[#f5f5f5] w-[100%] flex-col">
+        <div className="h-96 flex-2">
+          <img
+            className="h-full w-full object-cover"
+            src="images/BD-E-Torch-x-Dozo-B2SH_Desktop-Banner.png"
+            alt=""
+          />
+        </div>
+        <div className="h-96 w-1/4">
+          <img
+            className=" w-full h-full object-cover object-left-bottom"
+            src="images/BD-FireDeals_Mobile.png"
+            alt=""
+          />
+        </div>
+      </div>
+      {
+        <div className="flex gap-4 p-4 xl:hidden bg-[#f5f5f5] w-[100%] flex-col">
+          <div className="w-full">
+            <img
+              className="h-full w-full object-cover"
+              src="images/BD-E-Torch-x-Dozo-B2SH_Desktop-Banner.png"
+              alt=""
+            />
+          </div>
+          <div className="flex gap-4">
             <div className="w-full">
-              <img className="h-full w-full object-cover" src="images/BD-E-Torch-x-Dozo-B2SH_Desktop-Banner.png" alt="" />
+              <img
+                className="object-contain"
+                src="images/BD-FireDeals_Mobile.png"
+                alt=""
+              />
             </div>
-            <div className="flex gap-4">
-                <div className="w-full">
-                  <img className="object-contain" src="images/BD-FireDeals_Mobile.png" alt="" />
-                </div>
-                <div className="w-full">
-                  <img className="object-contain" src="images/BD-FireDeals_Mobile.png" alt="" />
-                </div>
+            <div className="w-full">
+              <img
+                className="object-contain"
+                src="images/BD-FireDeals_Mobile.png"
+                alt=""
+              />
             </div>
           </div>
-        }
-        
-        <Icons />
+        </div>
+      }
 
-        <NewArrival />
+      <Icons />
+
+      <NewArrival />
 
       <Subscribe />
     </main>
